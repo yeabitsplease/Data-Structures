a7515ace54549cf4f6be4a0b848fcdea2c95b4e3
Govind Saxena <govindsaksenaji@gmail.com>
2020-05-22 17:52:56 +0530

    Update InsertionSort.java

--- Sorting/InsertionSort.java
+++ Sorting/InsertionSort.java
@@ -1,22 +1,32 @@
+/*
+
+Space Complexity: O(1)
+Time Complexity: O(n^2)  {Worst Case}
+                 O(n)   {Best Case}
+
+*/
+
 import java.util.*;
 public class InsertionSort {
     
-    static void insertionSort(int[] x){
+    public static int[] insertionSort(int[] x){
     
+        if(x.length==0)
+            return new int[] {};
+        
         for(int i=1;i<x.length;i++){
-        int value=x[i];
-        int hole=i;
-        while(hole>0&& x[hole-1]>x[hole]){
         
-            x[hole]=x[hole-1];
-            hole--;
-            x[hole]=value;
-            
-        }
-        
+            int j=i;
+            while(j>0 && x[j-1]>x[j]){
+            int temp=x[j-1];
+            x[j-1]=x[j];
+            x[j]=temp;
+            j--;
+            }
         
         }
-    
+        return x;
+        
     }
     
     static void printArray(int[] arr){
@@ -34,13 +44,13 @@ public class InsertionSort {
         Scanner sc=new Scanner(System.in);
         System.out.println("Enter the number of entries in the array: ");
         int n=sc.nextInt();
-        int a[]=new int[n];
+        int array[]=new int[n];
         for(int i=0;i<n;i++){
-        a[i]=sc.nextInt();
+        array[i]=sc.nextInt();
         }
     
-        insertionSort(a);
-        printArray(a);
+        insertionSort(array);
+        printArray(array);
     }
     
     
